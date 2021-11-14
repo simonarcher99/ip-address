83fd4c0ef8721d18340ed5af11544a3d7d4c2dee
Simon Archer <simon.james.archer@gmail.com>
2021-11-14 11:10:15 +0000

    Moving API key to variables file

---
+++ src/components/LocationMap.js
@@ -0,0 +1,15 @@
+import Card from "../UI/Card";
+import { GCP_MAP_API_KEY } from "../variables/Variables";
+
+const LocationMap = (props) => {
+  return (
+    <Card>
+      <img
+        src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=${GCP_MAP_API_KEY}`}
+        alt=""
+      ></img>
+    </Card>
+  );
+};
+
+export default LocationMap;
