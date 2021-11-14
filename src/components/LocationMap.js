a4ea3b9933f8b84fb592e9ba264d8704b7987153
Simon Archer <simon.james.archer@gmail.com>
2021-11-14 12:18:01 +0000

    getting rid of key import

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,11 +1,11 @@
 import Card from "../UI/Card";
-import { GCP_MAP_API_KEY } from "../variables/Variables";
+// import { GCP_MAP_API_KEY } from "../variables/Variables";
 
 const LocationMap = (props) => {
   return (
     <Card>
       <img
-        src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=${GCP_MAP_API_KEY}`}
+        src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=`}
         alt=""
       ></img>
     </Card>
